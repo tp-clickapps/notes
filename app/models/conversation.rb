c1dfba53a81ff4c10fd13ab0aadde2d0a6c96d90
Tajinder Pal Singh <tajinder@clickapps.co>
2016-03-19 15:18:45 +0530

    Added Annotate and Conversation model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,18 +1,12 @@
 # == Schema Information
 #
-# Table name: users
+# Table name: conversations
 #
 #  id         :integer          not null, primary key
 #  name       :string
-#  email      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
-require 'test_helper'
-
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class Conversation < ActiveRecord::Base
 end
--- test/models/conversation_test.rb
+++ test/models/conversation_test.rb
@@ -1,17 +1,16 @@
 # == Schema Information
 #
-# Table name: users
+# Table name: conversations
 #
 #  id         :integer          not null, primary key
 #  name       :string
-#  email      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
+class ConversationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
